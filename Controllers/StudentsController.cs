d41e369b2a7f9722a9776362f4b575f6eefa4242
Karina-Abjima <karina.gwalani@abjima.com>
2023-05-30 11:16:51 +0530

    Updated everything with mapper

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -76,8 +76,7 @@ namespace Assignment_CFA.Controllers
                 
         }
 
-        // PUT: api/Students/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+       
         [HttpPut("{id}")]
         public async Task<IActionResult> PutStudent(Guid id, Students student)
         {
